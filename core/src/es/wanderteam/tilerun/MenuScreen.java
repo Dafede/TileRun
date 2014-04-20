fc20354b3be1f6aa1a5460eab6aac99581065702
ryuzaki <ojizaki@gmail.com>
2014-04-20 16:15:00 +0200

    Added MenuScreen, ScoreScreen, OptionsScreen, SplashScren. Added buttons to MenuScreen without feedback. Added simple transition between MenuScreen and the others

---
+++ core/src/es/wanderteam/tilerun/MenuScreen.java
@@ -0,0 +1,167 @@
+package es.wanderteam.tilerun;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+public class MenuScreen implements Screen{
+	Stage stage;
+	Skin uiSkin;
+	
+	Image background;
+	ImageButton ibPlay;
+	ImageButtonStyle ibsPlay;
+	ImageButton ibOptions;
+	ImageButtonStyle ibsOptions;
+	ImageButton ibScore;
+	ImageButtonStyle ibsScore;
+	
+	@Override
+	public void show() {
+		
+		
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+		TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("UI.pack"));
+		uiSkin = new Skin(atlas);
+		
+		//Create and add background
+		background = new Image(uiSkin.getDrawable("mainScreen"));
+		stage.addActor(background);
+		
+		//Create the style of the play button, create the playbutton whit that style,
+		//positioning the play button and adding it to the stage
+		ibsPlay = new ImageButtonStyle();
+		ibsPlay.imageUp = uiSkin.getDrawable("buttonPlay");
+		
+
+		
+		ibPlay = new ImageButton(ibsPlay);
+		ibPlay.setName("UI_PLAY_BUTTON");
+		ibPlay.setX((Gdx.graphics.getWidth() - ibPlay.getWidth()) / 2);
+		ibPlay.setY((Gdx.graphics.getHeight() * 1.5f  - ibPlay.getHeight()) / 2);
+		ibPlay.addListener(new ClickListener(){
+
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y,
+					int pointer, int button) {
+				return super.touchDown(event, x, y, pointer, button);
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y,
+					int pointer, int button) {
+				((Game)Gdx.app.getApplicationListener()).setScreen(new PlayScreen());
+				super.touchUp(event, x, y, pointer, button);
+			}
+			
+		});
+		
+		stage.addActor(ibPlay);
+		
+		//Create the style of the play button, create the options button whit that style,
+		//positioning the play button and adding it to the stage
+		ibsOptions = new ImageButtonStyle();
+		ibsOptions.imageUp = uiSkin.getDrawable("buttonOptions");
+		
+		ibOptions = new ImageButton(ibsOptions);
+		ibOptions.setX((Gdx.graphics.getWidth() - ibOptions.getWidth()) / 2);
+		ibOptions.setY((Gdx.graphics.getHeight() * 0.5f - ibOptions.getHeight()) / 2);
+		ibOptions.addListener(new ClickListener(){
+
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y,
+					int pointer, int button) {
+				// TODO Auto-generated method stub
+				return super.touchDown(event, x, y, pointer, button);
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y,
+					int pointer, int button) {
+				((Game)Gdx.app.getApplicationListener()).setScreen(new OptionsScreen());
+				super.touchUp(event, x, y, pointer, button);
+			}
+			
+		});
+		
+		stage.addActor(ibOptions);
+		
+		//Create the style of the play button, create the score button whit that style,
+		//positioning the play button and adding it to the stage
+		ibsScore = new ImageButtonStyle();
+		ibsScore.imageUp = uiSkin.getDrawable("buttonScore");
+		
+		ibScore = new ImageButton(ibsScore);
+		ibScore.setX((Gdx.graphics.getWidth() - ibScore.getWidth()) / 2);
+		ibScore.setY((Gdx.graphics.getHeight() - ibScore.getHeight()) / 2);
+		ibScore.addListener(new ClickListener(){
+
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y,
+					int pointer, int button) {
+				// TODO Auto-generated method stub
+				return super.touchDown(event, x, y, pointer, button);
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y,
+					int pointer, int button) {
+				((Game)Gdx.app.getApplicationListener()).setScreen(new ScoreScreen());
+				super.touchUp(event, x, y, pointer, button);
+			}
+			
+		});
+		stage.addActor(ibScore);
+		
+		
+	}
+	
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(47f / 255f, 67f / 255f, 244f / 255f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- core/src/es/wanderteam/tilerun/SplashScreen.java
+++ core/src/es/wanderteam/tilerun/SplashScreen.java
@@ -1,21 +1,54 @@
 package es.wanderteam.tilerun;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
 
 public class SplashScreen implements Screen {
-	SpriteBatch batch;
+	Stage stage;
+	
+	Image logo;
 	
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
-		batch = new SpriteBatch();
+		stage = new Stage();
+		
+		//Create, positionate and add to the stage the logo of the company
+		Texture logoTexture = new Texture(Gdx.files.internal("logo.png"));
+		logo = new Image(logoTexture);
+		logo.setX((Gdx.graphics.getWidth() - logoTexture.getWidth()) / 2);
+		logo.setY((Gdx.graphics.getHeight() - logoTexture.getHeight()) / 2);
+		stage.addActor(logo);
+		
+		//When the logo has been showed enough time, go to the menu screen
+		Timer.schedule(new Task() {
+			
+			@Override
+			public void run() {
+				stage.addAction(Actions.sequence(Actions.fadeOut(0.1f),Actions.run(new Runnable() {
+					
+					@Override
+					public void run() {
+						((Game)Gdx.app.getApplicationListener()).setScreen(new MenuScreen());
+					}
+				})));
+			}
+		}, 0.1f);
 	}
 	
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
-
+		Gdx.gl.glClearColor(91f / 255f, 200f / 255f, 165f / 255f, 0);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
 	}
 
 	@Override
@@ -45,7 +78,7 @@ public class SplashScreen implements Screen {
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-
+		stage.dispose();
 	}
 
 }
